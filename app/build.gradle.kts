22e5d326a36c3b363a460c809f8ad36d91e6fe84
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-11-21 09:34:43 +0100

    InterfazYLogin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.iesfranciscodelosrios.thatmovie"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.iesfranciscodelosrios.thatmovie"
---
+++ app/src/main/java/com/iesfranciscodelosrios/thatmovie/Home.java
@@ -0,0 +1,4 @@
+package com.iesfranciscodelosrios.thatmovie;
+
+public class Home {
+}
